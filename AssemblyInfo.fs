c1fa24bc5fcffe0346896d300f24aa7687303384
Goswin <gwinsky@yahoo.com>
2020-04-28 14:16:32 +0200

    init on 34.0.1

---
+++ AssemblyInfo.fs
@@ -0,0 +1,10 @@
+﻿namespace FsiRhinoTest.AssemblyInfo
+
+
+open System.Runtime.InteropServices
+
+// Without this Guid Rhino does not remeber the plugin after restart, setting <ProjectGuid> in the new SDK fsproj file does not to work.
+[<assembly: Guid("fbae86d1-3fe3-4d07-8cd7-3ac37147a3c8")>] 
+
+
+do ()
---
+++ PlugIn.fs
@@ -0,0 +1,63 @@
+﻿namespace FsiRhinoTest
+
+open Rhino
+open System
+open System.Windows
+open Rhino.Runtime
+open FSharp.Compiler.Interactive.Shell
+open System.IO
+open System.Diagnostics
+
+
+module Rh = 
+    let print a    = RhinoApp.WriteLine a    ; RhinoApp.Wait()
+
+
+type SeffTestPlugin () =  
+
+    inherit PlugIns.PlugIn()
+
+    static member val Instance = SeffTestPlugin() // singelton pattern     
+
+    override this.OnLoad refErrs = 
+            Rh.print  "loaded FsiRhinoTest Plugin"   
+            PlugIns.LoadReturnCode.Success
+  
+  
+
+  type FsiRhinoTestInitCmd () = // a dummy command just to make the plugin load
+      inherit Commands.Command()    
+     
+      static member val Instance = FsiRhinoTestInitCmd() 
+      
+      override this.EnglishName = "FsiRhinoTestInit" 
+             
+      override this.RunCommand (doc, mode)  =
+          Rh.print  "FsiRhinoTestInit ran."
+          Commands.Result.Success
+  
+
+  type FsiRhinoTestCreateSessionCmd () = 
+      inherit Commands.Command()    
+      
+      static member val Instance = FsiRhinoTestCreateSessionCmd() 
+      
+      override this.EnglishName = "FsiRhinoTestCreateSession" 
+             
+      override this.RunCommand (doc, mode)  =
+          Rh.print  "FsiRhinoTestCreateSessionCmd start..."
+          
+  
+          let inn = new StringReader("")
+          let out = new RhinoApp.CommandLineTextWriter()
+          let config = FsiEvaluationSession.GetDefaultConfiguration()
+          let noArgs = [| "the first arg is ignored "|]
+          let session = FsiEvaluationSession.Create(config, noArgs, inn, out, out)
+
+  
+          Rh.print  "Hurray! FsiEvaluationSession.Created!" 
+
+          session.EvalInteraction("let theAnswer = 40 + 2")
+
+          Rh.print  "is 42 the answer?" 
+          Commands.Result.Success
